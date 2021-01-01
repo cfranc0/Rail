54136a0970355b58c95f5277012ee78fa9245b0c
Leonardo <leonardopinton157@gmail.com>
2021-01-01 19:47:28 +0100

    Correzioni su RailLine.h e RailLine.cpp

--- Line/RailLine.cpp
+++ Line/RailLine.cpp
@@ -6,117 +6,119 @@
 
 using namespace std;
 
-RailLine_pice_standard::RailLine_pice_standard(int _from, int _to) {
+RailLine_piece_standard::RailLine_piece_standard(int _from, int _to) {
 	from = _from;
 	to = _to;
+	pieceType = 0;
 	doesBlockTraffic = true;
-	piceType = 0;
 	railSpeedLimit = { -1, -1 };
 }
 
-RailLine_pice_approach::RailLine_pice_approach(int _from, int _to, int type) {
+RailLine_piece_approach::RailLine_piece_approach(int _from, int _to, int type) {
 	from = _from;
 	to = _to;
+	pieceType = 1;
 	doesBlockTraffic = true;
-	piceType = 1;
 	if(type == 0)
 		railSpeedLimit = { 80, 80, 80, 80 };
 	else
 		railSpeedLimit = { 80, 80, 80, 80, -1, -1};
 }
 
-RailLine_pice_station::RailLine_pice_station(int _from, int _to, int type) {
+RailLine_piece_station::RailLine_piece_station(int _from, int _to, int type) {
 	from = _from;
 	to = _to;
+	pieceType = 2;
 	doesBlockTraffic = true;
-	piceType = 2;
 	if (type == 0)
 		railSpeedLimit = { 80, 80, 80, 80 };
 	else
 		railSpeedLimit = { 80, 80, 80, 80, -1, -1 };
 }
 
-RailLine_pice_parking::RailLine_pice_parking(int _from, int _to, int type) {
+RailLine_piece_parking::RailLine_piece_parking(int _from, int _to, int type) {
 	from = _from;
 	to = _to;
-	doesBlockTraffic = false;
-	piceType = 3;
+	pieceType = 3;
 	if (type == 0)
 		railSpeedLimit = { 80, 80, 80, 80 };
 	else
 		railSpeedLimit = { 80, 80, 80, 80, -1, -1 };
 }
 
-int RailLine_pice::getFrom() const{
+int RailLine_piece::getFrom() const{
 	return from;
 }
 
-int RailLine_pice::getTo() const{
+int RailLine_piece::getTo() const{
 	return to;
 }
 
-int RailLine_pice::getPiceType() const{
-	return piceType;
+int RailLine_piece::getPieceType() const{
+	return pieceType;
 }
 
-const vector<int> RailLine_pice::getRailSpeedLimit() const{
+const vector<int> RailLine_piece::getRailSpeedLimit() const{
 	return railSpeedLimit;
 }
 
-const std::list<Train*> RailLine_pice::getCurrentTrains() const{
+const std::list<Train*> RailLine_piece::getCurrentTrains() const{
 	return currentTrains;
 }
 
-const list<RailLine_pice> RailLine::getLine() const {
+const list<RailLine_piece> RailLine::getLine() const {
 
 	return line;
 }
 
 void RailLine::generateLine(list<Station> stations) {
 	list<Station>::iterator r = stations.begin();
-	list<Station>::iterator prev;
+	list<Station>::iterator prev = stations.begin();
 	++r;
-	for (r; r != stations.end(); ++r){
-		prev = --r;
-		line.push_back(RailLine_pice_approach(prev->getKm(), prev->getKm() + 5, prev->getType()));
-		line.push_back(RailLine_pice_parking(prev->getKm() + 5, prev->getKm() + 5, prev->getType()));
-		line.push_back(RailLine_pice_standard(prev->getKm() + 5, prev->getKm() + 5 + (r->getKm() - 5)));
-		line.push_back(RailLine_pice_parking(r->getKm() - 5, r->getKm() - 5, r->getType()));
-		line.push_back(RailLine_pice_approach(r->getKm() - 5, r->getKm(), r->getType()));
-		line.push_back(RailLine_pice_station(r->getKm(), r->getKm(), r->getType()));
+	for (r; r != stations.end(); ++r, ++prev){
+		line.push_back(RailLine_piece_approach(prev->getKm(), prev->getKm() + 5, prev->getType()));
+		line.push_back(RailLine_piece_parking(prev->getKm() + 5, prev->getKm() + 5, prev->getType()));
+		line.push_back(RailLine_piece_standard(prev->getKm() + 5, r->getKm() - 5));
+		line.push_back(RailLine_piece_parking(r->getKm() - 5, r->getKm() - 5, r->getType()));
+		line.push_back(RailLine_piece_approach(r->getKm() - 5, r->getKm(), r->getType()));
+		line.push_back(RailLine_piece_station(r->getKm(), r->getKm(), r->getType()));
 	}
 }
 
-list<Train*> RailLine::whosThere(int km_from, int km_to) {
-	list<Train*> t;
-	return t;
-}
-
-const vector<int> RailLine::getSpeedLimit(int km){
-	vector<int> speedLimit;
-	list<RailLine_pice>::iterator t = line.begin();
-	for (t;  t != line.end(); ++t){
-		if(t->getFrom() == km || t->getTo() == km){
-			speedLimit = t->getRailSpeedLimit();
-			break;
-		}
-		if(t->getPiceType() == 1){
-			for (int i = 1; i < 6; ++i) {
-				if(t->getFrom() + i == km){
-					speedLimit = t->getRailSpeedLimit();
-					break;
+list<Train*> RailLine::whosThere(int km_from, int km_to) { // WORK IN PROGRESS
+	list<Train*> trains;
+	list<RailLine_piece>::iterator l = line.begin();
+	for (l; l != line.end(); ++l) {
+		if(l->getTo() >= km_from && l->getFrom() - km_to > 0){
+				list<Train*> trainsTemp = l->currentTrains();
+				list<Train*>::iterator t = trainsTemp.begin();
+				for (t; t != trainsTemp.end(); ++t) {
+					trains.push_back(*t);
 				}
 			}
 		}
-		else
-			speedLimit = t->getRailSpeedLimit();
+	return t;
+}
+
+list<RailLine_piece>::iterator RailLine::getRailLinePiece(int km){
+
+	list<RailLine_piece>::iterator t = line.begin();
+	for (t;  t != line.end(); ++t){
+		if(t->getFrom() <= km && t->getTo() >= km)
+			break;
 	}
-	return speedLimit;
+	return t;
+}
+
+const std::vector<int> RailLine::getSpeedLimit(int km){
+
+	list<RailLine_piece>::iterator t = getRailLinePiece(km);
+	return t->getRailSpeedLimit();
 }
 
 ostream& operator<<(ostream& os, const RailLine& _line) {
-	list<RailLine_pice> _l = _line.getLine();
-	list<RailLine_pice>::iterator lineIt = _l.begin();
+	list<RailLine_piece> _l = _line.getLine();
+	list<RailLine_piece>::iterator lineIt = _l.begin();
 	for(lineIt; lineIt != _l.end(); ++lineIt)
 	{
 		os << lineIt->getFrom() << " " << lineIt->getTo();
--- Line/RailLine.h
+++ Line/RailLine.h
@@ -8,25 +8,24 @@
 #include "Station.h"
 #include "Train.h"
 
-class RailLine_pice {
+class RailLine_piece {
 
 public:
 	int getFrom() const; // return the initial point of a pice of line
 	int getTo() const; // return the ending point of a pice of line
-	int getPiceType() const; // return the type of the pice of the line
+	int getPieceType() const; // return the type of the piece of the line
 	const std::vector<int> getRailSpeedLimit() const; // Return the vector with the speed limit of that pice of line
 	const std::list<Train*> getCurrentTrains() const; // Return the list with the current trains on a pice of line
 
 protected:
-	RailLine_pice() {}; // The access to the default constructor is blocked
+	RailLine_piece() {}; // The access to the default constructor is blocked
 
 	std::list<Train*> currentTrains; // list of current trains in a pice of line
 	std::vector<int> railSpeedLimit; // speed limit for the tracks
 	int from = 0; // Initial point of the line
 	int to = 0; // Ending point of the line
-	int piceType = 0;
+	int pieceType = 0;
 	bool doesBlockTraffic = false; // check if a train on the line will take up space
-
 };
 
 class RailLine {
@@ -34,31 +33,32 @@ class RailLine {
 public:
 	void generateLine(std::list<Station> stations); // Generate a line
 	std::list<Train*> whosThere(int km_from, int km_to); // Return a list of train that are between the specified Kms
+	std::list<RailLine_piece>::iterator getRailLinePiece(int km);
 	const std::vector<int> getSpeedLimit(int km);
-	const std::list<RailLine_pice> getLine() const; // Return the line
+	const std::list<RailLine_piece> getLine() const; // Return the line
 
 private:
-	std::list<RailLine_pice> line; // Contains all the pices that form a line
+	std::list<RailLine_piece> line; // Contains all the pices that form a line
 };
 
-class RailLine_pice_standard : public RailLine_pice { //Create a standard pice of line
+class RailLine_piece_standard : public RailLine_piece { //Create a standard pice of line
 public:
-	RailLine_pice_standard(int from, int to);
+	RailLine_piece_standard(int from, int to);
 };
 
-class RailLine_pice_approach : public RailLine_pice { //Create an approach pice of line
+class RailLine_piece_approach : public RailLine_piece { //Create an approach pice of line
 public:
-	RailLine_pice_approach(int from, int to, int type);
+	RailLine_piece_approach(int from, int to, int type);
 };
 
-class RailLine_pice_station : public RailLine_pice { //Create a station pice of line
+class RailLine_piece_station : public RailLine_piece { //Create a station pice of line
 public:
-	RailLine_pice_station(int from, int to, int type);
+	RailLine_piece_station(int from, int to, int type);
 };
 
-class RailLine_pice_parking : public RailLine_pice { //Create a parking pice of line
+class RailLine_piece_parking : public RailLine_piece { //Create a parking pice of line
 public:
-	RailLine_pice_parking(int from, int to, int type);
+	RailLine_piece_parking(int from, int to, int type);
 };
 
 std::ostream& operator<<(std::ostream& os, const RailLine& _line); // print the line
