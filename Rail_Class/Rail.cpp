a793488fde63911e430a4974bac4a1374db92205
Carlotta <carlotta.schiavo.1@studenti.unipd.it>
2021-01-03 19:51:02 +0100

    Rail con loadTimeTable terminata+ loadLine terminata + aggiunta metodi getter richiesti

--- Rail_Class/Rail.cpp
+++ Rail_Class/Rail.cpp
@@ -7,24 +7,24 @@
 #include "Rail.h"
 using namespace std;
 
-void Rail::loadLine(string ld){
+void Rail::loadLine(string ld) {
 	//Apertura file
 	string fileName = ld;
 	ifstream ist{ fileName };
-	if (!ist) throw exception{"File non trovato"};
-	
+	if (!ist) throw exception{ "File non trovato" };
+
 	string s = " ";//linea appena letta
 	string temp = "";//varibile di supporto
 	int tmp = 0;//varibile di supporto
-	
+
 	//inserimento stazione d'origine
 	string nameOrig = "";//nome della stazione d'origine
 	getline(ist, nameOrig);
-	Main_station mainStat(nameOrig,0);
+	Main_station mainStat(nameOrig, 0);
 	stationsList.push_back(mainStat);
-	
+
 	while (getline(ist, s)) {
-		
+
 		istringstream istd(s);
 		vector<string>pieceOfReadLine;
 		vector<int>num;
@@ -42,19 +42,20 @@ void Rail::loadLine(string ld){
 		}
 
 		//creazione oggetti Station e inserimento in lista
-		if(num[1]==0){
-			Main_station m(statName,num[0]);
+		if (num[1] == 0) {
+			Main_station m(statName, num[0]);
 			stationsList.push_back(m);
 		}
-		else{
-			Local_station l(statName,num[0]);
+		else {
+			Local_station l(statName, num[0]);
 			stationsList.push_back(l);
 		}
 	}//while
 	ist.close();
 	line.generateLine(stationsList);
 }//loadLine
-//DA TERMINARE
+
+
 void Rail::loadTimeTable(string tt) {
 	//Apertura file
 	string fileName = tt;
@@ -63,113 +64,122 @@ void Rail::loadTimeTable(string tt) {
 
 	string s = " ";//linea appena letta
 	int tmp = 0;//varibile di supporto
-
+	int token_vec_size = stationsList.size() + 3;
 	while (getline(ist, s)) {
 		istringstream istd(s);
-		vector<int>token(stationsList.size());//in questo modo ciascun elemento del vettore si inizializza a zero
+		vector<int>token(token_vec_size);//in questo modo ciascun elemento del vettore si inizializza a zero
 		int count = 0;
+
 		//riempimento vettore con token linea appena letta
-		while (istd >> tmp) {
+		while (istd >> tmp && count < token_vec_size) {
 			token[count] = tmp;
 			count++;
 		}
 
+		//inizializzazione variabili che serviranno per il calcolo dell'orario
 		int ID = token[0];
 		int direc = token[1];
 		int train_type = token[2];
-		int departure_t = token[3];
+		int departure_t = token[3];//orario di partenza da stazione d'origine
 
-		//stationsList
-		list<Station>::iterator it;
+		list<Station>::iterator it;//iteratore per la lista di stazioni
 		int i = 0;
-		int count_stat = 3;
-		list<int>timeTablelist;
 
-		//controllo stazione d'origine
-		/*it = stationsList.begin();
-		int arrival_t = token[count_stat];
-		int duration_min = departure_t - arrival_t;
-		int duration_hour = duration_min/ 60;
-		int distance_km = it->getKm();
-		int time = calculateTime(train_type, it->getType(), arrival_t, duration_hour, distance_km, 0);
-		if (time < duration_min) {
-			timeTablelist.push_back(time);
-		}
-		else {
-			timeTablelist.push_back(duration_min);
-		}
-		next(it);
-		while (it != prev(stationsList.end())) {
+		/*count_stat--> tiene conto della stazione della quale stiamo verificando l'orario d'arrivo
+		  parte da 3 perchè gli orari di arrivo partono da terzo elemento del vector
+		  che o riempito con gli elementi letti da ciuscuna riga del file*/
+		int count_stat = 3;
+		list<int>timeTablelist;//lista degli orari (una per ciascun treno)
+
+		int count_not_hour = 0;//varibile che serve a verificare se la timetable ha degli orari mancanti;
+		int arrival_t;
+		int duration_min;
+		int duration_hour;
+		int distance_km;
+		int time;
+
+		for (it = stationsList.begin(); it != stationsList.end() && count_stat < token_vec_size; ++it) {
 			arrival_t = token[count_stat];
-			duration_min = departure_t - arrival_t;
-			duration_hour = duration_hour / 60;
+			//cout << arrival_t<<" ";
+			duration_min = arrival_t - departure_t;
+
+			//ogni volta che trova un orario mancante, incrementa la variabile che conta gli orari mancanti
+			if (arrival_t == 0) {
+				count_not_hour++;
+			}
+
 			distance_km = it->getKm();
 
-			time = calculateTime(train_type, it->getType(), arrival_t, duration_hour, distance_km, 1);
-			if (time < duration_min) {
-				timeTablelist.push_back(time);
+			/*controlla se la stazione sia "terminale" o che si trovi lungo la linea*/
+			if (count_stat == 3 || count_stat == token_vec_size - 1) {
+				time = calculateTime(train_type, it->getType(), distance_km, 0);
 			}
 			else {
-				timeTablelist.push_back(duration_min);
+				time = calculateTime(train_type, it->getType(), distance_km, 1);
 			}
-			next(it);
-			count_stat++;
-		}
-		//stazione arrivo
-		arrival_t = token[count_stat];
-		duration_min = departure_t - arrival_t;
-		duration_hour = duration_hour / 60;
-		distance_km = it->getKm();
-		time = calculateTime(train_type, it->getType(), arrival_t, duration_hour, distance_km, 0);
-		if (time < duration_min) {
+
+			/*controllo della validità dell'orario di arrivo*/
+			if (duration_min < time || arrival_t == 0) {
+				time = time + departure_t;
+			}
+			else {
+				time = duration_min + departure_t;
+			}
+			/*se ci sono stati orari mancanti, (quindi count_not_hour>0), vengono
+			aggiunti 10min all'orario di arrivo al "capolinea" )*/
+			if (count_stat == token_vec_size - 1 && count_not_hour > 0) {
+				time += 10;
+			}
 			timeTablelist.push_back(time);
-		}
-		else {
-			timeTablelist.push_back(duration_min);
-		}
+			count_stat++;
+		}//for
+
 		Train* t = createTrain(train_type, ID, direc, timeTablelist);
-		trainsList.push_back(t);*/
+		trainsList.push_back(t);
+
 	}//while
+
+	cout << trainsList.size();
 }
 
-/*int Rail::calculateTime(int train_t, int station_t, int arr_t, int dur_h, int dist_km,int isTerminal){
-	int time = 0;
-	const int STOP_DUR = 5 / 60; //5min/60
-	int max_sp = getMaxSpeedFromType(train_t);
 
-	if ((train_t == 0 || ((train_t == 2 || train_t == 3) && station_t==0)) && isTerminal==1) {
-		time = ((10 / 80) + ((dist_km - 10) / max_sp) + (STOP_DUR))*60;
+int Rail::calculateTime(int train_t, int station_t, int dist_km, int isTerminal) {
+	double time = 0;
+	const double STOP_DUR = 5 / 60; //5min/60
+	double max_sp = getMaxSpeedFromType(train_t);
+
+	if ((train_t == 1 || ((train_t == 2 || train_t == 3) && station_t == 0)) && isTerminal == 1) {
+		time = ((10 / 80) + ((dist_km - 10) / max_sp) + (STOP_DUR)) * 60;
 	}
-	else{
-		time = ((10 / 80) + ((dist_km - 10) / max_sp));
+	else {
+		time = ((10 / 80) + ((dist_km - 10) / max_sp)) * 60;
 	}
 
-	return time;
-}*/
+	return int(time);
+}
 
-/*int Rail::getMaxSpeedFromType(int train_t) {
-	if (train_t == 0) {
+double Rail::getMaxSpeedFromType(int train_t) {
+	if (train_t == 1) {
 		return 160;
 	}
-	else if (train_t == 1) {
-		 return 240;
+	else if (train_t == 2) {
+		return 240;
 	}
 	return 300;
-}*/
+}
 
-/*Train* Rail::createTrain(int type, int ID, int direc, list<int>& time) {
+Train* Rail::createTrain(int type, int ID, int direc, list<int>& time) {
 	if (type == 1) {
-		Regional t(ID,direc,time);
+		Regional t(ID, direc, time);
 		Train* tr = &t;
 		return tr;
 	}
 	else if (type == 2) {
-		High_speed t(ID,direc,time);
+		High_speed t(ID, direc, time);
 		Train* tr = &t;
 		return tr;
 	}
-	High_speed_super t(ID, direc,time);
+	High_speed_super t(ID, direc, time);
 	Train* tr = &t;
 	return tr;
-	
-}*/
+}
--- Rail_Class/Rail.h
+++ Rail_Class/Rail.h
@@ -20,44 +20,82 @@
 #include <exception>
 
 class Rail {
-	private:
-		RailLine line;
-		std::list<Station>stationsList;
-		std::list<Train*>trainsList;
-		//DA TERMINARE
-		/*funzioni di utilita'*/
-		int calculateTime(int train_t, int station_t, int arr_t, int dur_h, int dist_km,int isTerminal);
-		int getMaxSpeedFromType(int type);
-		Train* createTrain(int type,int ID,int direc,std::list<int>& time);
-	public:
-		/**
-		 *loadLine
-		 *funzione membro che legge da file i dati sulle stazioni
-		 *crea gli oggetti stazione e li carica in stationsList.
-		 *Riempita la lista, invoca il metodo "generateLine" della classe
-		 *RailLine che andrà a generare la linea ferroviaria.
-		 * 
-		 *@param ld, il file "line_description.txt"
-		 */
-		void loadLine(std::string ld);
-		
+private:
+	RailLine line;
+	std::list<Station>stationsList;
+	std::list<Train*>trainsList;
 
-		/**
-		 *loadLine
-		 *funzione membro che legge da file i dati della TimeTable.
-		 *crea gli oggetti Treni e li carica in trainsList.
-		 *Prima di creare l'oggetto Treno, la funzione controlla che i dati del file
-		 *siano conformi, se necessario li modifica.
-		 *
-		 *@param ld, il file "line_description.txt"
-		 */
-		void loadTimeTable(std::string tt);
+	/*funzioni di utilita'*/
 
-		//void simulate();
+	/**
+	 * calculateTime
+	 * calcola i tempi esatti che dovrebbe impiegare il treno ad arrivare ad un certa stazione.
+	 * @param train_t, tipo del treno
+	 * @param station_t, tipo della stazione
+	 * @param dist_km, distanza dell' n-esima stazione dalla stazione d'origine
+	 * @param isTerminal, 0=origine o capolinea, 1=stazione in mezzo alla linea;
+	 * @return il tempo calcolato
+	 */
+	int calculateTime(int train_t, int station_t, int dist_km, int isTerminal);
 
-		/*FUNZIONI CHE NON SO SE POSSONO SERVIRE MA CHE INTANTO USO PER IL TEST*/
-		std::list<Station> getStationsList() { return stationsList; }
-		std::list<Train*> getTrainsList() { return trainsList; }
+	/**
+	 * getMaxSpeedFrom
+	 * @param type, tipo di treno
+	 * @return Velocita' massimo raggiungibile da treno
+	 */
+	double getMaxSpeedFromType(int type);
+
+	/**
+	 * createTrain
+	 * funzione che crea un treno (a seconda del tipo indicato)
+	 * @param type
+	 * @param ID
+	 * @param direc
+	 * @param time
+	 * @return Train*
+	 */
+	Train* createTrain(int type, int ID, int direc, std::list<int>& time);
+public:
+
+	/**
+	 *loadLine
+	 *funzione membro che legge da file i dati sulle stazioni
+	 *crea gli oggetti stazione e li carica in stationsList.
+	 *Riempita la lista, invoca il metodo "generateLine" della classe
+	 *RailLine che andrà a generare la linea ferroviaria.
+	 *@param ld, il file "line_description.txt"
+	 */
+	void loadLine(std::string ld);
+
+
+	/**
+	 *loadLine
+	 *funzione membro che legge da file i dati della TimeTable.
+	 *crea gli oggetti Treni e li carica in trainsList.
+	 *Prima di creare l'oggetto Treno, la funzione controlla che i dati del file
+	 *siano conformi, se necessario li modifica.
+	 *
+	 *@param ld, il file "line_description.txt"
+	 */
+	void loadTimeTable(std::string tt);
+
+
+	/**
+	 * getStationsList
+	 * @return stationsList
+	*/
+	std::list<Station> getStationsList()const { return stationsList; }
+	/**
+	 * getTrainsList
+	 * @return trainsList
+	*/
+	std::list<Train*> getTrainsList()const { return trainsList; }
+
+	/**
+	 * getLine
+	 * @return line
+	*/
+	RailLine getLine() const { return line; }
 
 };
 
